6d7def215810b6b8b6418a5960a8141569667a13
SangameshSJ <jssangamesh01@gmail.com>
2025-03-04 18:06:56 +0530

    updated applications

--- applications/app2/app2.py
+++ applications/app2/app2.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Application 2</h1><p>This is deployed in subnet 2</p>'
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    username = request.form.get('username')
+    return f'<h1>Hello, {username}!</h1><p>Thank you for submitting the form.</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
