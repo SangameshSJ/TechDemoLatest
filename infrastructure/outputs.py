90c399972cfd81428cfdbe3b7c9be4541846a0c6
SangameshSJ <jssangamesh01@gmail.com>
2025-03-04 16:32:09 +0530

    pipeline update

--- infrastructure/outputs.py
+++ infrastructure/outputs.py
@@ -1,18 +1,23 @@
-# infrastructure/outputs.py
 import pulumi
 import json
 
 def export_outputs(vpc, bastion_instance, jenkins_instance, app1_instance, app2_instance, key_name):
-    outputs = {
-        "vpc_id": vpc.id,
-        "bastion_public_ip": bastion_instance.public_ip,
-        "jenkins_public_ip": jenkins_instance.public_ip,
-        "app1_private_ip": app1_instance.private_ip,
-        "app2_private_ip": app2_instance.private_ip,
-        "ssh_command": pulumi.Output.concat("ssh -i ", key_name, ".pem ec2-user@", bastion_instance.public_ip)
-    }
-    
-    # Export outputs to a JSON file
-    pulumi.export("outputs", outputs)
+    # Use `pulumi.Output.all` to resolve multiple outputs at once
+    resolved_outputs = pulumi.Output.all(
+        vpc_id=vpc.id,
+        bastion_public_ip=bastion_instance.public_ip,
+        jenkins_public_ip=jenkins_instance.public_ip,
+        app1_private_ip=app1_instance.private_ip,
+        app2_private_ip=app2_instance.private_ip,
+        ssh_command=pulumi.Output.concat("ssh -i ", key_name, ".pem ec2-user@", bastion_instance.public_ip)
+    )
+
+    # Export the resolved outputs
+    resolved_outputs.apply(lambda outputs: pulumi.export("outputs", outputs))
+
+    # Write the resolved outputs to a JSON file
+    resolved_outputs.apply(lambda outputs: write_outputs_to_file(outputs))
+
+def write_outputs_to_file(outputs):
     with open("outputs.json", "w") as f:
         json.dump(outputs, f)
