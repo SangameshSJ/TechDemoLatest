a476a06837162abcdc64f710e482a5af4aebd016
SangameshSJ <jssangamesh01@gmail.com>
2025-04-01 12:05:34 +0530

    production env

--- infrastructure/loadbalancer.py
+++ infrastructure/loadbalancer.py
@@ -135,6 +135,8 @@ def create_load_balancer(vpc_id, public_subnet_ids, app_instance_ids, security_g
     Returns:
         dict: Dictionary containing all created load balancer resources
     """
+    config = pulumi.Config()
+    environment = config.get("environment")
 
     alb_sg = aws.ec2.SecurityGroup("alb-sg",
         vpc_id=vpc_id,
